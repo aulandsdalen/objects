5500a6b9dbe909a579bdf2734a7881a01b216c28
Алёна Мирабо <quantumpeppermint@Quantum-Peppermint.local>
2017-01-23 11:02:18 +0300

    object-oriented rewrite

--- client.rb
+++ client.rb
@@ -1,41 +1,50 @@
 require 'socket'
 
+class Client 
 
-def parse_reply(reply)
-	reply.split(";")
+
+	@id = nil
+	@epoch_start = nil
+	@tick_duration = nil
+	@reference_frame = {
+		:epoch_start => @epoch_start,
+		:tick_duration => @tick_duration
+	}
+
+
+	def initialize(addr, port, tick_duration)
+		_r = send_request(addr, port, "uninitialized;id")
+		@id = _r[0]
+		@epoch_start = Time.now.to_i
+		@tick_duration = tick_duration
+		@reference_frame = {
+			:epoch_start => @epoch_start,
+			:tick_duration => @tick_duration
+		}
+	end
+
+	def status
+		puts "my id = #{@id}"
+		puts "my reference_frame = #{@reference_frame}"
+	end
+
+	def tick
+		# do something awesome here
+	end
+
+	private
+	def parse_reply(reply)
+		reply.split(";")
+	end
+
+
+	def send_request(addr, port, request)
+		s = TCPSocket.open(addr, port)
+		s.puts request
+		_reply = s.gets
+		s.close
+		parse_reply(_reply)
+	end
+
+
 end
-
-
-def send_request(addr, port, request)
-	s = TCPSocket.open(addr, port)
-	s.puts request
-	reply = s.gets
-	s.close
-	parse_reply(reply)
-end
-
-addr = "0.0.0.0"
-port = "5555"
-id = 0
-
-epoch_start = Time.now.to_i
-tick_duration = 2
-
-reference_frame = {
-	:epoch_start => epoch_start,
-	:tick_duration => tick_duration
-}
-
-
-puts "Starting at #{addr}:#{port}"
-
-### initialize -- get ID from server ###
-
-reply_array = send_request(addr, port, "uninitialized;id")
-id = reply_array[0]
-
-puts "received #{id}"
-
-
-### clean up on exit ###
-
